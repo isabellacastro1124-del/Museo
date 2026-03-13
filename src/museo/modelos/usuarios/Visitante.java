2d407727c4bf67a31e6d31afe921b47e4e3ef705
isabella <isabellacastro1124@gmail.com>
2026-03-13 14:06:53 -0500

    feat: clase Visitante para consultar obras disponibles por sala

---
+++ src/museo/modelos/usuarios/Visitante.java
@@ -0,0 +1,11 @@
+package museo.modelos.usuarios;
+
+import museo.modelos.obras.ObraArte;
+import java.util.List;
+
+public class Visitante {
+
+    public List<ObraArte> consultarObrasPorSala(String sala) {
+        return null;
+    }
+}
