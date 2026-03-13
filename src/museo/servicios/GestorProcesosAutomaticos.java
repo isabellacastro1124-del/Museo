f1a7879dd5cbbb54742ae993db2768150262b211
isabella <isabellacastro1124@gmail.com>
2026-03-13 14:06:59 -0500

    feat: servicio para ejecutar la revision diaria de restauracion sobre todas las obras

---
+++ src/museo/servicios/GestorProcesosAutomaticos.java
@@ -0,0 +1,10 @@
+package museo.servicios;
+
+import museo.modelos.obras.ObraArte;
+import java.util.List;
+
+public class GestorProcesosAutomaticos {
+
+    public void ejecutarRevisionDiariaRestauracion(List<ObraArte> obras) {
+    }
+}
