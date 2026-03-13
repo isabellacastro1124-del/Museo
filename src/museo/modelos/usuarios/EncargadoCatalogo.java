4749962605f6ed96491083fdd0f0217baeadd187
isabella <isabellacastro1124@gmail.com>
2026-03-13 14:06:36 -0500

    feat: clase EncargadoCatalogo para registrar y actualizar obras en el catalogo

---
+++ src/museo/modelos/usuarios/EncargadoCatalogo.java
@@ -0,0 +1,16 @@
+package museo.modelos.usuarios;
+
+import museo.modelos.obras.ObraArte;
+
+public class EncargadoCatalogo extends Usuario {
+
+    public EncargadoCatalogo(String username, String clave) {
+        super(username, clave);
+    }
+
+    public void registrarObra(ObraArte obra) {
+    }
+
+    public void actualizarDatosObra(ObraArte obra) {
+    }
+}
