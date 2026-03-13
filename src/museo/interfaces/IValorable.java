b26822923c101eb0d54636a3752b8d602be358da
isabella <isabellacastro1124@gmail.com>
2026-03-13 14:05:50 -0500

    feat: interfaz para obtener el valor economico de una obra

---
+++ src/museo/interfaces/IValorable.java
@@ -0,0 +1,13 @@
+package museo.interfaces;
+
+/**
+ * Interfaz para elementos que tienen un valor económico.
+ */
+public interface IValorable {
+    /**
+     * Obtiene el valor económico del elemento.
+     * 
+     * @return El valor en formato double.
+     */
+    double obtenerValor();
+}
