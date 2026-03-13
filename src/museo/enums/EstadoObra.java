40373c12f52ce27337538237963b88381bc5b5ef
isabella <isabellacastro1124@gmail.com>
2026-03-13 14:04:13 -0500

    feat: enum con los posibles estados de una obra de arte

---
+++ src/museo/enums/EstadoObra.java
@@ -0,0 +1,11 @@
+package museo.enums;
+
+/**
+ * Enumeración que representa los posibles estados de una Obra de Arte.
+ */
+public enum EstadoObra {
+    EXPUESTA,
+    EN_RESTAURACION,
+    CEDIDA,
+    DANADA
+}
